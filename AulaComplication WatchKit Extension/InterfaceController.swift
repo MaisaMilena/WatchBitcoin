a49b340685d3a067a53433f2fa659b23c39d6a48
Maisa <maisa.milena@gmail.com>
2017-09-20 00:00:00 -0300

    exercício finalizado

--- AulaComplication WatchKit Extension/InterfaceController.swift
+++ AulaComplication WatchKit Extension/InterfaceController.swift
@@ -9,31 +9,12 @@
 import WatchKit
 import Foundation
 
-protocol CotacaoDelegate{
-    func cotacaoDelegateConcluida(cotacaoDolar: Double, cotacaoBitcoin: Double)
-}
-
-
-class InterfaceController: WKInterfaceController, LoadCotacaoPresenterDelegate {
-
-    // MARK: - Delegates
-    var loader: LoaderPresenter?
-    var cotacaoDelegate: CotacaoDelegate?
-    
-    // MARK: - Valor das cotações
-    var cotacaoDolar: Double!
-    var cotacaoBitcoin: Double!
+class InterfaceController: WKInterfaceController {
     
     // MARK: - Funções principais
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        loader = LoaderPresenter()
-        loader?.delegate = self
-        loader?.getCotacao(from: .BTC)
-        loader?.getCotacao(from: .USD)
-        
-        //cotacaoDelegate?.cotacaoDelegateConcluida(cotacaoDolar: cotacaoDolar, cotacaoBitcoin: cotacaoBitcoin)
+
     }
     
     override func willActivate() {
@@ -44,20 +25,5 @@ class InterfaceController: WKInterfaceController, LoadCotacaoPresenterDelegate {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
-    }
-    
-    // MARK: - Funções do Load Presenter
-    func loadCotacaoConcluido(cotacao: Double, moeda: Moeda){
-        if moeda == Moeda.USD {
-            cotacaoDolar = cotacao
-        } else {
-            cotacaoBitcoin = cotacao
-        }
-        
-        print("Print no Load Cotacao no InterfaceController, valor: \(cotacao), moeda: \(moeda)")
-    }
-    
-    func loadCotacaoFalhou(mensagem: String){
-        print("Ocorreu um problema com a conexão com o servidor 🙁")
     }
 }
